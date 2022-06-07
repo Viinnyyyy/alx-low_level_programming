a7cbd11ab26a2778bfb4781416b39661eb3fab84
Viinnyyyy <akinlabi.aderibigbe@stu.cu.edu.ng>
2022-06-07 01:50:55 -0700

    6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (Success)
+ *
+ **/
+int main(void)
+{
+	int n;
+
+
+	for (n = 0; n < 10;)
+	{
+		putchar(n % 10 + '0');
+		n++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
