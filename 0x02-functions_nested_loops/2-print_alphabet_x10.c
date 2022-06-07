f45182cdc54de72afda9ff0d8c3edae97af3cb48
Viinnyyyy <akinlabi.aderibigbe@stu.cu.edu.ng>
2022-06-07 16:39:53 -0700

    2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+
+#include <stdio.h>
+/**
+ *main - check the code
+ *
+ *Return: Always 0.
+ **/
+int main(void)
+{
+	char c;
+	int count = 0;
+
+	while (count < 10)
+	{
+
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			putchar('c');
+		}
+		putchar('\n');
+	}
+	return (0);
+}
