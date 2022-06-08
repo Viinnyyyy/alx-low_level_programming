41bd4f4c9aacf7766803413de64e15538e50746f
Viinnyyyy <akinlabi.aderibigbe@stu.cu.edu.ng>
2022-06-07 18:49:31 -0700

    1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,17 +5,15 @@
  *
  *Return: Always 0.
  **/
-int main(void)
+void print_alphabet(void);
 {
-	int i;
-	for (i = 0; i < 10; i++)
-	{
-		while (i < 10)
-		{
-		print_alphabet();
-		}
+	int l
 
-	_putchar('\n');
+	for (l = 'a'; i <= 'z'; i++)
+	{
+		print_alphabet();
 	}
+	_putchar('\n');
+	
 	return (0);
 }
