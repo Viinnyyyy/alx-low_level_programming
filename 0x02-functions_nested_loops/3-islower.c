f1c70006827419744c1ff01053afa235ab65fc54
Viinnyyyy <akinlabi.aderibigbe@stu.cu.edu.ng>
2022-06-07 19:53:44 -0700

    3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *_islower - check the code
+ *
+ *Return: char.
+ **/
+int _islower(int c)
+{
+	char c;
+
+	if ( islower(c))
+	{
+		return(1);
+	}
+	else
+		return(0);
+}
