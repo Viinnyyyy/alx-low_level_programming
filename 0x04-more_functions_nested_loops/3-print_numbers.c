8e9b1acb4a9140bd18e7149b8e8b9791079d401a
Viinnyyyy <akinlabi.aderibigbe@stu.cu.edu.ng>
2022-06-09 19:05:29 -0700

    3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *print_numbers - function
+ *
+ *@a: number
+ *Return: void
+ */
+void print_numbers(void)
+{
+	int a = 0;
+
+	while (a < 10)
+	{
+		_putchar(a);
+		a++;
+	}
+	_putchar('\n');
+}
