164f276f4a2f0018799e350e157eff0048e08b0c
Viinnyyyy <akinlabi.aderibigbe@stu.cu.edu.ng>
2022-06-09 18:27:26 -0700

    1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *_isdigit - something
+ *
+ *@c: character
+ *Return: return integer
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 0 && c < 10)
+	{
+		return (1);
+	}
+	return (0);
+}
