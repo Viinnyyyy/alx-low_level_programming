dae3b0c52f619e9fd16384abaa341407afb505fe
Viinnyyyy <akinlabi.aderibigbe@stu.cu.edu.ng>
2022-06-13 12:01:59 -0700

    2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *_strlen - returns string length
+ *
+ *@s: string value
+ *
+ *Return: length
+ */
+int _strlen(char *s)
+{
+	int len;
+
+	len = strlen(*s);
+
+	return (len);
+}
