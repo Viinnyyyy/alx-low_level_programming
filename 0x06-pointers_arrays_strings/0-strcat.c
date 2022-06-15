419ecb920545f963ba8b1ed25e975b41adf3d4e5
Viinnyyyy <akinlabi.aderibigbe@stu.cu.edu.ng>
2022-06-15 03:27:26 -0700

    0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ **_strcat - concatenate strings
+ *
+ *@dest: destination
+ *@src: source
+ *Return: char
+ */
+char *_strcat(char *dest, char *src)
+{
+	size_t dest_len = strlen(dest);
+	size_t i;
+	size_t n = strlen(src);
+
+	for (i = o; i < n && src[i] != '\0'; i++)
+	{
+		dest[dest_len + i] = src[i];
+	}
+	dest[dest_len + i] = '\0'
+
+	return (dest)
+}
