dce953fb481322cd307c19e0478f60c7d07fe74c
Viinnyyyy <akinlabi.aderibigbe@stu.cu.edu.ng>
2022-06-16 00:12:47 -0700

    1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stddef.h>
+#include <stdio.h>
+#include <string.h>
+/**
+ **_strncat - concatenate strings
+ *
+ *@dest: destination
+ *@src: source
+	 *Return: char
+	 */
+char *_strncat(char *dest, char *src, int n)
+{
+	size_t dest_len = strlen(dest);
+	size_t i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[dest_len + i] = src[i];
+	}
+	dest[dest_len + i] = '\0';
+
+	return (dest);
+}
