ddaf90b55b2c86f84fad5936aadff62c96580329
Viinnyyyy <akinlabi.aderibigbe@stu.cu.edu.ng>
2022-06-16 20:34:14 -0700

    5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -10,9 +10,12 @@ char *string_toupper(char *str)
 {
 	int i;
 
-	for (i = 0; str[i] >= 'a' && str[i] <= 'z'; i++)
+
+	i = 0;
+	while (str[i] >= 'a' && str[i] <= 'z')
 	{
-		str[i] = str[i] - 32;
+		str[i] -= 32;
+		i++;
 
 	}
 	return (str);
