c54dc8b73fa009596ea8d6c89e1d8a69c5ca15a8
Viinnyyyy <akinlabi.aderibigbe@stu.cu.edu.ng>
2022-06-17 09:18:54 -0700

    6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -13,7 +13,6 @@ char *cap_string(char *str)
 	{
 		while (str[i] >= 'a' && str[i] <= 'z')
 		{
-			i++;
 			if (str[i - 1] == '\t' ||
 				str[i - 1] == '\n' ||
 				str[i - 1] == ' ' ||
@@ -26,7 +25,7 @@ char *cap_string(char *str)
 				str[i - 1] == '(' ||
 				str[i - 1] == ')' ||
 				str[i - 1] == '{' ||
-				str[i - 1] == '}' || i == 0)
+				str[i - 1] == '}')
 			{
 				str[i] -= 32;
 			}
