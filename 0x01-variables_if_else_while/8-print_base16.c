865c72cc5713b70e31e23befddeaddc17a565ac3
Viinnyyyy <akinlabi20001@gmail.com>
2022-09-11 14:06:11 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -4,22 +4,19 @@
  *main - Entry point
  *
  *Return: Always 0 (Success)
- *
  **/
 int main(void)
 {
 	int n;
-	int hex;
 
-
-	for (n = 0; n < 10; n++)
+	for (n = 0; n <= 9; n++)
 	{
-		putchar(n % 10 + '0');
+		putchar(n + '0');
 	}
 
-	for (hex = 'a'; n <= 'f'; hex++)
+	for (n = 97; n <= 102; n++)
 	{
-		putchar(hex);
+		putchar(n);
 	}
 
 	putchar('\n');
