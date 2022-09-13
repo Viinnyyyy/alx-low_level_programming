90e09819af2048d441b2109decb2b51489528278
Viinnyyyy <akinlabi20001@gmail.com>
2022-09-13 10:56:00 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ *_abs - check the code
+ *@int: changes any number to positive
+ *
+ *Return: char.
+ **/
+int _abs(int);
+{
+	int n;
+
+	if (n > 0)
+	{
+		_putchar(n);
+	}
+	else if (n == 0)
+	{
+		_putchar(n);
+	}
+	else
+	{
+		n = n * -1;
+		_putchar(n);
+	}
+
+	return (n);
+}
