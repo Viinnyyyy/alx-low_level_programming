f48cf4cf548b27cf6f03c961e3be92ced2c3e064
Viinnyyyy <akinlabi20001@gmail.com>
2022-09-13 10:39:47 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,23 +8,21 @@
  **/
 int print_sign(int n)
 {
-	int x;
-
-	if (x > 0)
+	if (n > 0)
 	{
-		x = 1;
+		n = 1;
 		_putchar('+');
 	}
-	else if (x == 0)
+	else if (n == 0)
 	{
-		x = 0;
+		n = 0;
 		_putchar('0');
 	}
 	else
 	{
-		x = -1;
+		n = -1;
 		_putchar('-');
 	}
 
-	return(x);
+	return(n);
 }
