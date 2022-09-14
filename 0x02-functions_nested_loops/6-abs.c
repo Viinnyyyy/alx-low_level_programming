8bb6cff5a08a81946877412c2db16ff6ecbf3c89
Viinnyyyy <akinlabi20001@gmail.com>
2022-09-13 17:29:57 -0700

    6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,13 +2,11 @@
 
 /**
  *_abs - check the code
- *@int: changes any number to positive
+ *@n: changes any number to positive
  *
  *Return: int.
  **/
-int _abs(int)
+int _abs(int n)
 {
-	int n;
-
 	return (n >= 0 ? n : n * -1);
 }
