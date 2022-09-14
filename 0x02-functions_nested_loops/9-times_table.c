07570843ed0d3b15bb3b74d4c8dcbfda8b1e6f77
Viinnyyyy <akinlabi20001@gmail.com>
2022-09-13 18:32:11 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,11 +12,11 @@ void times_table(void)
 	int n = 0;
 	int times = a * n;
 
-	for (a = 0; a < 10; a++)
+	while (a <= 9)
 	{
 		while (n < 10)
 		{
-			_putchar(times);
+			_putchar('0' + times);
 			_putchar(',');
 			_putchar(' ');
 			_putchar(' ');
@@ -25,6 +25,7 @@ void times_table(void)
 			n++;
 		}
 		_putchar('\n');
+		a++;
 	}
 
 }
