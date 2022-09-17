0a528f0beb77bfba3094e95fb799f4311d6d8fd4
Viinnyyyy <akinlabi20001@gmail.com>
2022-09-16 20:13:49 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_line - prints line
+ *
+ * @n: line number
+ * Return: void
+ */
+
+void print_line (int n)
+{
+	while (n-- > 0)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,3 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ * main - header file
+ * void prototype (void)
+ * int prototype (int)
+ */
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
@@ -10,3 +17,4 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
+#endif /*MAIN_H*/
