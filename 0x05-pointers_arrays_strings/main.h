ce6dbb935043eb0512d9d9c7619007401ca8a351
Viinnyyyy <akinlabi20001@gmail.com>
2022-09-19 21:37:20 -0700

    9-strcpy.c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,3 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_h
+/**
+ * main - header file
+ * void prototype(void)
+ * int protoype(int)
+ */
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -5,3 +12,5 @@ int _putchar(char c);
 void print_rev(char *s);
 void _puts(char *s);
 void rev_string(char *s);
+char *_strcpy(char *dest, char *src);
+#endif /*MAIN_H*/
