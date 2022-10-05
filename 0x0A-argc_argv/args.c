9d6b3db6033aabf9c04dea265c8b01535506fdf2
Viinnyyyy <akinlabi20001@gmail.com>
2022-10-04 18:26:09 -0700

    omo

---
+++ 0x0A-argc_argv/args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main -  self explanatory
+ *@argc: argument count
+ *@argv: argument vector
+ *Return: number of arguments passed
+ */
+int main(int argc, char *argv[])
+{
+	*argv[] = ' ';
+	if (argc > 1)
+	{
+		printf("%d\n", argc - 1);
+	}
+	return(argc);
+}
+
