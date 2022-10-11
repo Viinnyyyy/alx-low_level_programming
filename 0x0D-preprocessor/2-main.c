c407b7510fd95bc7826d01a0210f29511aaf6e86
Viinnyyyy <akinlabi20001@gmail.com>
2022-10-10 19:45:35 -0700

    2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+#define FILE_NAME __FILE__
+
+/**
+ * main - Function thst prints file name
+ *
+ *Return: void
+ */
+int main(void)
+{
+	printf('%s\n', __FILE__);
+	return (0);
+}
+
